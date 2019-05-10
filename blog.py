40cdb4de70060307a9254f09df55aaed84d0fa36
amponia <50479356+amponia@users.noreply.github.com>
2019-05-10 09:47:28 +0200

    Add files via upload

---
+++ blog.py
@@ -0,0 +1,29 @@
+from post import Post
+
+class Blog:
+    def __init__(self, title: object, author: object) -> object:
+        self.title = title
+        self.author = author
+        self.posts = []
+
+    def __repr__(self):
+        return "Blog Title by Blog Author (0 posts)"
+        return '{} by {} ({} post{})'.format(
+            self.title,
+            self.author,
+            len(self.posts),
+            's' if len(self.posts) != 1
+            else '')
+
+    def create_post (self, title, content):
+        self.posts.append(Post(title,content))
+
+    def json (self):
+        return {
+            'title': self.title,
+            'author': self.author,
+            'posts': [post.json() for post in self.posts]
+        }
+
+
+
